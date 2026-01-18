f7bbe53b3df1265387d29e54a4e5b03d3b5dc3b4
JadielSilva11 <jdielcunha@gmail.com>
2026-01-17 21:33:40 -0300

    Exercício 4

---
+++ Exercícios Básicos/exe4.py
@@ -0,0 +1,4 @@
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+
+print(f"A soma de a e b: {a + b}\nA subtração de a e b: {a - b}\nA multiplicação de a e b: {a * b}\nA divisão de a e b: {a / b}")
