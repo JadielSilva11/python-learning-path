5332a1bf5dbeb11483d16013a13c855cfc2d2206
JadielSilva11 <jdielcunha@gmail.com>
2026-01-18 10:58:32 -0300

    Exercício 12

---
+++ Exercícios Básicos/exe12.py
@@ -0,0 +1,14 @@
+def ler_temperatura():
+    tempC = int(input("Digite a temperatura em Celsius: "))
+    return tempC
+
+def converter_temp(tempC):
+    tempF = (9 * tempC + 160) / 5
+    return tempF
+
+
+c = ler_temperatura()
+f = converter_temp(c)
+
+print(f"Temperatura em Celsius: {c}")
+print(f"Temperatura em Fahrenheit: {f}")
