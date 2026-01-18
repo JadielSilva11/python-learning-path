561f7c4cbeff70c23b657f3cf7334bc20f1da518
JadielSilva11 <jdielcunha@gmail.com>
2026-01-17 22:34:33 -0300

    Exercício 10

---
+++ Exercícios Básicos/exe10.py
@@ -0,0 +1,11 @@
+lista = [0] * 5
+
+for i in range(5):
+    lista[i] = int(input("> "))
+
+soma = 0
+for i in range(5):
+    soma += lista[i]
+
+print(f"\nElementos da lista: {lista}\n")
+print(f"Soma dos elementos da lista: {soma}\n")
