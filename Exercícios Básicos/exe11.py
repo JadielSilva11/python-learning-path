93d6719f7f6e1935da51f1acb60021c3bc9ac069
JadielSilva11 <jdielcunha@gmail.com>
2026-01-17 22:34:51 -0300

    Exercício 11

---
+++ Exercícios Básicos/exe11.py
@@ -0,0 +1,16 @@
+dic = {}
+
+for i in range(3):
+    nome = str(input("Digite o nome do aluno: "))
+    nota = float(input("Digite a nota do aluno: "))
+    dic[nome] = nota
+
+cont = 0
+soma = 0
+for nome in dic:
+    soma += dic[nome]
+    cont += 1
+
+media = soma / cont
+
+print(f"Media das notas dos alunos: {round(media, 1)}")
